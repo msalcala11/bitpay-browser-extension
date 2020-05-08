dfe0a3421577800b1631a9a3b13170f52529818b
Marty Alcala <marty@bitpay.com>
2020-05-08 17:47:40 -0400

    safe pathname

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,11 +1,18 @@
 import track from 'react-tracking';
 
+function getSafePathname(pathname: string): string {
+  const parts = pathname.split('/');
+  const invoiceIdIndex = 2;
+  const safeParts = [...parts.slice(0, invoiceIdIndex), ...parts.slice(invoiceIdIndex + 1)];
+  return pathname.startsWith('/card') ? `${safeParts.join('/')}` : pathname;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function trackComponent(component: React.FC<any>, eventProperties: any = {}): React.FC<any> {
   return track(
     props => ({
-      pathname: props.location.pathname,
-      ...eventProperties
+      ...eventProperties,
+      ...(props.location && props.location.pathname && { pathname: getSafePathname(props.location.pathname) })
     }),
     eventProperties.page ? { dispatchOnMount: true } : {}
   )(component);
