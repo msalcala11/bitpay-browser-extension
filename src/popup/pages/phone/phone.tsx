d08aa5282803134e36435c36a01fb6b283d8a8c0
Marty Alcala <marty@bitpay.com>
2020-08-12 18:57:19 -0400

    add secondary button

--- src/popup/pages/phone/phone.tsx
+++ src/popup/pages/phone/phone.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/no-autofocus */
 import './phone.scss';
 import React, { useRef, useEffect, Dispatch, SetStateAction } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import { resizeToFitPage } from '../../../services/frame';
 import { trackComponent } from '../../../services/analytics';
+import ActionButton from '../../components/action-button/action-button';
 
 const Phone: React.FC<RouteComponentProps & {
   phone?: string;
@@ -12,7 +13,7 @@ const Phone: React.FC<RouteComponentProps & {
   console.log(phone, setPhone);
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    resizeToFitPage(ref, 49);
+    resizeToFitPage(ref, 170);
   }, [ref]);
   return (
     <div className="settings account">
@@ -31,6 +32,14 @@ const Phone: React.FC<RouteComponentProps & {
             </div>
             <div className="settings-group__caption">Only a United States phone number can be used</div>
           </div>
+        </div>
+        <div className="action-button__footer--fixed">
+          <Link to={{ pathname: `/payment/` }}>
+            <ActionButton>Enable Contactless</ActionButton>
+          </Link>
+          <Link to={{ pathname: `/payment/` }}>
+            <div className="secondary-button">Skip</div>
+          </Link>
         </div>
       </div>
     </div>
