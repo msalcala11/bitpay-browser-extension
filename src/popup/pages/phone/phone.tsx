3883237fe177c1a394bbfba3d7fa3f97978d3322
Marty Alcala <marty@bitpay.com>
2020-08-12 17:39:22 -0400

    prelim phone input

--- src/popup/pages/phone/phone.tsx
+++ src/popup/pages/phone/phone.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-autofocus */
 import './phone.scss';
 import React, { useRef, useEffect, Dispatch, SetStateAction } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
@@ -18,7 +19,18 @@ const Phone: React.FC<RouteComponentProps & {
       <div ref={ref}>
         <div className="account__zero-state">
           <div className="account__title">Enable Contactless?</div>
-          <div className="account__body">Add a phone number to connect this card to Apple Pay or Google Pay.</div>
+          <div className="account__body" style={{ marginBottom: 0 }}>
+            Add a phone number to connect this card to Apple Pay or Google Pay.
+          </div>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+          <div className="settings-group">
+            <div className="settings-group__label">Phone Number</div>
+            <div className="settings-group__input">
+              <input type="tel" defaultValue={phone} placeholder="Phone Number" autoFocus required />
+            </div>
+            <div className="settings-group__caption">Only a United States phone number can be used</div>
+          </div>
         </div>
       </div>
     </div>
